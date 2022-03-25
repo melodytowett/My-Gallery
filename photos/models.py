6c30a7de4b5e8982dd9174e5ec7d16b091035faa
melodytowett <melodytowett99@gmail.com>
2022-03-25 15:39:52 +0300

    add models

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,15 @@
+from email.mime import image
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    
+    name = models.CharField(max_length=35)
+    description=models.CharField(max_length=200)
+
+class Category(models.Model):
+    name = models.CharField(max_length=35)
+
+class Location(models.Model):
+    name = models.CharField(max_length=35)
