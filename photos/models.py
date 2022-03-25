d628b29890b3925f21f8f1640ac3b0dbfb01ec0c
melodytowett <melodytowett99@gmail.com>
2022-03-25 18:39:46 +0300

    writing tests for models

--- photos/models.py
+++ photos/models.py
@@ -1,15 +1,35 @@
-from email.mime import image
-from unicodedata import name
+
 from django.db import models
 
 # Create your models here.
-class Image(models.Model):
-    
-    name = models.CharField(max_length=35)
-    description=models.CharField(max_length=200)
-
 class Category(models.Model):
     name = models.CharField(max_length=35)
 
+    def __str__(self):
+        return self.name
+    
+    def save_cat(self):
+        self.save()
+
+
 class Location(models.Model):
     name = models.CharField(max_length=35)
+
+    def __str__(self):
+        return self.name
+
+    def save_loc(self):
+        self.save()
+
+class Image(models.Model):
+    image = models.ImageField(upload_to ='photos/', blank=True)
+    name = models.CharField(max_length=35)
+    description=models.CharField(max_length=200)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    location = models.ForeignKey(Location,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()
--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
-
+from .models import Image,Category,Location
 # Create your tests here.
+class CategoryTestClass(TestCase):
+    def setUp(self):
+        self.category = Category(name='Nature')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.category,Category))
+    
+    #Test to save the method
+    def test_save_method(self):
+        self.category.save_cat()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) > 0)
+    
+
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.location = Location(name="Africa")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.location,Location))
+
+    def test_save_method(self):
+        self.location.save_loc()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
+
+
+class ImageTestClass(TestCase):
+    #setup mathod
+    def setUp(self):
+        self.image=Image(name='Desert',description='chalbi desert in Africa')
+
+    #Test Instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image,Image))
+
+    #test to save method
