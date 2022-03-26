1dde37733d7d5e1dae60f6d2c65d2626ba8a4fc7
melodytowett <melodytowett99@gmail.com>
2022-03-26 11:38:22 +0300

    add view funtion to display search results

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,4 @@
+from email import message
 from django.shortcuts import render
 
 import photos
@@ -7,3 +8,14 @@ def index(request):
     location = Location.objects.all()
     photos = Image.current_images(location)
     return render(request,'index.html', {"photos":photos})
+
+def search_results(request):
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_by_name(search_term)
+        message = "{search_term}"
+
+        return render(request,'all-photos/search.html',{"message":message,"photos":searched_images})
+    else:
+        message = "You havent searched fro any term"
+        return render(request,'all-photos/search.html',{"message":message})
